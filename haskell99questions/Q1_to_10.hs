84267aeb41133a393a9d42f029c4ea66c442544f
stubbs774 <charlie.stubbs@ntlworld.com>
2021-05-02 12:28:10 +0100

    commit

--- haskell99questions/Q1_to_10.hs
+++ haskell99questions/Q1_to_10.hs
@@ -1,5 +1,26 @@
 -- Q1
-myLast :: [a] -> [a]
-myLast [] = []
-myLast (x : []) = [x]
-myLast _ = []
+myLast :: [c] -> c
+myLast =  head . reverse
+
+
+-- P2
+myButLast :: [c] -> c
+myButLast = head . tail . reverse 
+
+--P3
+elementAt :: [c] -> Int  -> c
+elementAt xs n = xs !! (n - 1)
+
+--P4
+myLength :: [a] -> Int 
+myLength = sum . map(const 1)
+
+--P5
+myReverse :: [a] -> [a]
+myReverse = foldl (flip (:)) []
+
+
+--isPlaindrome
+isPalindrome' []  = True
+isPalindrome' [_] = True
+isPalindrome' xs  = (head xs) == (last xs) && (isPalindrome' $ init $ tail xs)
