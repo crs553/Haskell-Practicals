605c7bf5f1b8a0eb19b8d3b73a3187869091043f
stubbs774 <charlie.stubbs@ntlworld.com>
2021-05-11 22:20:43 +0100

    Setting.hs

--- noughtsAndCrosses/Setting.hs
+++ noughtsAndCrosses/Setting.hs
@@ -5,11 +5,27 @@ data Player = O | X deriving (Eq, Show)
 data PieceType = Placed | NotPlaced deriving(Eq)
 
 data Point = 
-    OneOne | OneTwo | OneThree | 
-    TwoOne | TwoTwo | TwoThree |
-    ThreeOne | ThreeTwo | ThreeThree deriving (Eq)
+    Sq11 | Sq12 | Sq13 | 
+    Sq21 | Sq22 | Sq23 |
+    Sq31 | Sq32 | Sq33 deriving (Eq)
+
+data Position = Start | Plcd Point deriving (Eq)
+
+data Piece = Piece {pos :: Maybe Point, plyr:: Player}
 
 getOppo:: Player -> Player
 getOppo O = X
 getOppo X = O
+
+placePiece :: Int -> Int -> Position
+placePiece 1 1 = Plcd Sq11
+placePiece 1 2 = Plcd Sq12
+placePiece 1 3 = Plcd Sq13
+placePiece 2 1 = Plcd Sq21
+placePiece 2 2 = Plcd Sq22
+placePiece 2 3 = Plcd Sq23
+placePiece 3 1 = Plcd Sq31
+placePiece 3 2 = Plcd Sq32
+placePiece 3 3 = Plcd Sq33
+placePiece _ _ = Start 
 
