641d3fb9ef229e93e62fcdf4cabb8c5ac1253f76
stubbs774 <charlie.stubbs@ntlworld.com>
2021-05-14 22:18:19 +0100

    final haskell question of the eve

--- haskell99questions/Q11_onwards.hs
+++ haskell99questions/Q11_onwards.hs
@@ -1,9 +1,26 @@
 module Q11_onwards where
 
---P14
-dupli :: [a] -> [a]
-dupli [] = []
-dupli (x:xs) = x : x : dupli xs
 
+--P14
 dupli' :: [b] -> [b]
 dupli' = concatMap (replicate 2)
+
+
+--P15
+repli ::  [a] -> Int -> [a]
+repli xs i = concatMap (replicate i) xs
+
+
+--P16
+dropEvery :: [b] -> Integer -> [b]
+dropEvery = flip $ \n -> map snd . filter ((n/=) . fst) . zip (cycle [1..n])
+
+
+--P17
+split' :: [a] -> Int -> ([a], [a])
+split' xs n = (take n xs, drop n xs)
+
+
+--P18
+slice :: [a] -> Int -> Int -> [a]
+slice xs i k | i>0 = take (k-i+1) $ drop (i-1) xs
